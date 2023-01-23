ac346c4af696b2fec9577fd94be99a7c6b6598e8
vaceslavkameronov <slka@mail.ru>
2023-01-23 22:35:22 +0400

    решение Задачи 1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        //task2();
+        //task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        //Массив 1
+        int[] one = new int [3];
+        one [0] = 1;
+        one [1] = 2;
+        one [2] = 3;
+        // Массив 2
+        double [] two = {1.57,7.654,9.986};
+        // массив 3
+        int [] buildingFloor = {1,2,3,4,5,6,7,8,9};
+    }
+
+
+}
+
