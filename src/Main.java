f82df8e90645f431c56b324a65058a87f13fc10a
vaceslavkameronov <slka@mail.ru>
2023-01-23 23:59:05 +0400

    решение Задачи 2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,80 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
-        //task3();
+        task2();
+        task3();
         //task4();
         //task5();
         //task6();
         //task7();
         //task8();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         //Массив 1
-        int[] one = new int [3];
-        one [0] = 1;
-        one [1] = 2;
-        one [2] = 3;
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
         // Массив 2
-        double [] two = {1.57,7.654,9.986};
+        double[] two = {1.57, 7.654, 9.986};
         // массив 3
-        int [] buildingFloor = {1,2,3,4,5,6,7,8,9};
+        int[] buildingFloor = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     }
+    public static void task2() {
+        System.out.println("Задача 2");
+        //Массив 1
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        for (int i = 0; i < one.length; i++) {
+            if (i !=2) {
+                System.out.print(one[i] + ", ");
+            } else {
+                System.out.print(one[i]);
+                System.out.println();
+            }
+        }
+        // Массив 2
+        double[] two = {1.57, 7.654, 9.986};
+        for (int i = 0; i < two.length; i++) {
+            if (i !=2) {
+                System.out.print(two[i] + ", ");
+            } else {
+                System.out.print(two[i]);
+                System.out.println();}
+        }
+        // массив 3
+        int[] buildingFloor = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < buildingFloor.length; i++) {
+            if (i != buildingFloor.length - 1) {
+                System.out.print(buildingFloor[i] + ", ");
+            } else {
+                System.out.print(buildingFloor[i]);
+                System.out.println();}
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        //Массив 1
+       int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        for (int i = 2; i >= 0; i--) {
+           if (i != 0) {
+                System.out.print(one[i] + ", ");
+            } else {
+                System.out.print(one[i]);
+                System.out.println();
+            }
+        }
+    }}
 
 
-}
+
+
+
 
